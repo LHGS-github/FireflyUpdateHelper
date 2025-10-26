4744f217124b330842d3b86e54032d32c641b0b8
LHGS_github <340994706@qq.com>
2025-10-26 09:00:01 +0800

    struct

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='firefly_update_helper',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "loguru",
+        "pyyaml"
+    ],
+    author='灵魂歌手er',
+    author_email='340994706@qq.com',
+    description='适用于Python项目的更新解决方案',
+    license='MIT',
+    keywords='sample setuptools development',
+    url='https://github.com/LHGS-github/FireflyUpdateHelper'
+)
